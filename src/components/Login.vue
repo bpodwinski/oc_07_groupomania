85866cf467df946db19c34535578c6f7ebead817
Benoit Podwinski <me@benoitpodwinski.com>
2020-10-22 12:53:47 +0200

    rewrite login form with VeeValidate

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,28 +1,64 @@
 <template>
   <v-container>
-    <v-card class="mx-auto my-12">
-      <v-form v-model="valid">
-        <v-container>
-          <v-row>
-            <v-col cols="12">
-              <v-card-title>Connexion</v-card-title>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field v-model="email" :rules="[rules.email]" label="E-mail" required></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field v-model="password" :rules="[rules.password]" type="password" label="Mot de passe" required></v-text-field>
-            </v-col>
-          </v-row>
+    <v-card class="mx-auto my-12" max-width="480" elevation="10">
+      <v-app-bar flat dark center color="indigo">
+        <v-toolbar-title>Connexion</v-toolbar-title>
+      </v-app-bar>
 
-          <span v-if="error">{{error}}</span>
+      <validation-observer ref="loginForm" v-slot="{ invalid }">
+        <v-form @submit.prevent="submit" class="px-5 py-2">
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="E-mail"
+                  rules="required|email|max:128"
+                >
+                  <v-text-field
+                    prepend-icon="mdi-email"
+                    :error-messages="errors"
+                    v-model="email"
+                    :counter="128"
+                    label="E-mail"
+                    required
+                  ></v-text-field>
+                </validation-provider>
+              </v-col>
+              <v-col cols="12">
+                <validation-provider
+                  v-slot="{ errors }"
+                  name="Mot de passe"
+                  rules="required|min:6|max:128"
+                >
+                  <v-text-field
+                    prepend-icon="mdi-lock"
+                    :error-messages="errors"
+                    v-model="password"
+                    :counter="128"
+                    type="password"
+                    label="Mot de passe"
+                    required
+                  ></v-text-field>
+                </validation-provider>
+              </v-col>
+            </v-row>
 
-          <v-card-actions>
-            <v-btn v-on:click.native="onSubmit" :disabled="!valid" color="success">Connexion</v-btn>
-          </v-card-actions>
+            <span v-if="error">{{ error }}</span>
 
-        </v-container>
-      </v-form>
+            <v-card-actions>
+              <v-btn
+                block
+                depressed
+                type="submit"
+                :disabled="invalid"
+                color="indigo lighten-2 white--text"
+                >Se connecter</v-btn
+              >
+            </v-card-actions>
+          </v-container>
+        </v-form>
+      </validation-observer>
     </v-card>
   </v-container>
 </template>
@@ -30,42 +66,56 @@
 <script lang="ts">
 import Vue from "vue";
 import { mapGetters, mapActions } from "vuex";
+import { required, email, min, max } from "vee-validate/dist/rules";
+import { extend, ValidationObserver, ValidationProvider } from "vee-validate";
+
+extend("required", {
+  ...required,
+  message: "Ne peux pas être vide",
+});
+
+extend("min", {
+  ...min,
+  message: "Doit avoir au moins {length} caractères",
+});
+
+extend("max", {
+  ...max,
+  message: "Ne doit pas dépasser {length} caractères",
+});
+
+extend("email", {
+  ...email,
+  message: "L'email doit être valide",
+});
 
 export default Vue.extend({
   name: "Login",
+
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
 
   data() {
     return {
       valid: true,
       email: null,
       password: null,
-      rules: {
-        firstname: v => /^[\s\w-éàëêâ]{2,23}$/.test(v) || "Le prénom est invalide",
-        lastname: v => /^[\s\w-éàëêâ]{2,23}$/.test(v) || "Le nom est invalide",
-        password: v =>
-          /^(?!.* )(?=.*\d)(?=.*[A-Z]).{8,24}$/.test(v) ||
-          "Le mot de passe est invalide",
-        confPassword: v =>
-          v === this.password || "Le mot de passe ne correspond pas",
-        email: v =>
-          /^\w{3,}@\w{3,}\.\w{2,3}$/.test(v) ||
-          "L'email est invalide, ex. d'e-mail valide: john@doe.com"
-      }
     };
   },
+
   methods: {
     ...mapActions(["Login"]),
-    onSubmit(e) {
-      e.preventDefault();
+    submit() {
+      this.$refs.loginForm.validate();
       this.Login({
         email: this.email,
-        password: this.password
+        password: this.password,
       });
     },
-    validate() {
-      this.$refs.form.validate();
-    }
   },
-  computed: mapGetters(["user", "error"])
+
+  computed: mapGetters(["user", "error"]),
 });
 </script>
