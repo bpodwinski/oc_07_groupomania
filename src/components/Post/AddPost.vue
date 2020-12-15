2bf77f5666255af8e0838cf9a66d627116cabd73
Benoit Podwinski <me@benoitpodwinski.com>
2020-12-15 13:42:02 +0100

    fix AddPost component

--- src/components/Post/AddPost.vue
+++ src/components/Post/AddPost.vue
@@ -9,11 +9,11 @@
               v-model="title"
               :label="$t('post.title')"
             ></v-text-field>
-            <v-textarea v-model="text" :label="$t('post.body')"></v-textarea>
+            <v-textarea v-model="content" :label="$t('post.body')"></v-textarea>
             <v-btn
               color="indigo lighten-2 white--text"
               type="submit"
-              @click="sendPost(userID, title, text)"
+              @click="sendPost(title, content)"
               >{{ $t("send") }}</v-btn
             >
           </v-col>
@@ -32,10 +32,9 @@ const post = namespace("post");
 @Component
 export default class AddPost extends Vue {
   // data
-  private userID: number = this.$store.getters["user"].userId;
   private data: object = {
     title: "",
-    text: "",
+    content: "",
   };
 
   // map action
@@ -43,11 +42,13 @@ export default class AddPost extends Vue {
   private addPost!: (post: object) => Promise<void | any>;
 
   // methods
-  public sendPost(userID: number, title: string, text: string): void {
-    this.addPost({ userID, title, text }).then(() => {
-      title = "";
-      text = "";
-    });
+  public sendPost(title: string, content: string): void {
+    const post = {
+      userId: this.$store.state.login.credentials.userId,
+      title: title,
+      content: content,
+    };
+    this.addPost(post);
   }
 }
 </script>
