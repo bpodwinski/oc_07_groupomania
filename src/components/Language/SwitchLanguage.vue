8f009cb0824dae60a2770d0838e2baa512d270f9
Benoit Podwinski <me@benoitpodwinski.com>
2020-12-15 13:41:38 +0100

    add SwitchLanguage to Language folder

---
+++ src/components/Language/SwitchLanguage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-select
+      class="switchlocale"
+      v-model="$i18n.locale"
+      :items="langs"
+      dense
+      outlined
+    ></v-select>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "SwitchLocale",
+  data() {
+    return { langs: ["en", "fr"] };
+  },
+};
+</script>
+
+<style lang="scss">
+.switchlocale {
+  margin: 25px 15px 0 0 !important;
+  max-width: 80px !important;
+}
+</style>
