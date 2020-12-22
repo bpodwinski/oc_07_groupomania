8f33869f405ee5659340c7fb7160dde7c09d892a
Benoit Podwinski <me@benoitpodwinski.com>
2020-12-22 17:02:56 +0100

    test authmenu

--- src/components/Nav/AuthMenu.vue
+++ src/components/Nav/AuthMenu.vue
@@ -3,7 +3,12 @@
     <v-menu bottom offset-y>
       <template v-slot:activator="{ attrs, on }">
         <v-btn text v-bind="attrs" v-on="on">
-          <v-gravatar :mail="account.data.email" alt="Avatar" :size="30" />
+          <v-avatar>
+            <img
+              :src="gravatar"
+              :alt="account.data.firstname + ' ' + account.data.lastname"
+            />
+          </v-avatar>
         </v-btn>
       </template>
 
@@ -12,12 +17,11 @@
           v-if="account.data.id"
           :to="{ name: 'Profile', params: { id: account.data.id } }"
         >
-          <v-list-item-title
-            ><v-icon left>mdi-account</v-icon>
-            {{ $t("profile") }}</v-list-item-title
-          >
+          <v-list-item-title>
+            <v-icon left>mdi-account</v-icon>
+            {{ $t("profile") }}
+          </v-list-item-title>
         </v-list-item>
-
         <v-list-item @click="logout">
           <v-list-item-title
             ><v-icon left>mdi-logout</v-icon>
@@ -38,11 +42,9 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import { namespace, State } from "vuex-class";
-import Gravatar from "vue-gravatar";
 
 const login = namespace("login");
 const account = namespace("account");
-Vue.component("v-gravatar", Gravatar);
 
 @Component
 export default class AuthMenu extends Vue {
@@ -54,6 +56,10 @@ export default class AuthMenu extends Vue {
 
   public logout() {
     this.Logout();
+  }
+
+  get gravatar() {
+    return this.account.data.gravatar + "?d=retro";
   }
 }
 </script>
