a34357e21568fcedc095a903f36360bdd8ef7c80
Benoit Podwinski <me@benoitpodwinski.com>
2020-12-25 20:38:19 +0100

    add Makefile
    Signed-off-by: Benoit Podwinski <me@benoitpodwinski.com>

---
+++ Makefile
@@ -0,0 +1,13 @@
+API_VER="1.6.0"
+
+export API_VER
+
+install:
+	npm install
+
+backend:
+	chmod +x ./utils/scripts/*
+	eval "npm run api $(API_VER)"
+
+dev:
+	npm run dev
--- utils/scripts/api.sh
+++ utils/scripts/api.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-
-API_VER=1.3.0
+set -ex
 
 rm -fr api/ && \
 mkdir api/ && \
-curl https://github.com/bpodwinski/oc_07_groupomania_api/releases/download/v${API_VER}/release.zip -L -o api.zip && \
+curl https://github.com/bpodwinski/oc_07_groupomania_api/releases/download/v$1/release.zip -L -o api.zip && \
 unzip api.zip -d ./ && \
 cp -r dist/* api/ && \
 rm -rf api.zip dist/
 
-# start API server
+# install npm modules
 cd api/
 npm install
 
 sleep 1
-echo API $API_VER successfully installed!
+echo "API $API_VER successfully installed!"
